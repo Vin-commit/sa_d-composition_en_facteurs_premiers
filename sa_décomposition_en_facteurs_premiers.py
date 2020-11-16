f5145375856fdb43859d44dd17de8aa48bb20a28
Vin-commit <74498336+Vin-commit@users.noreply.github.com>
2020-11-16 19:56:43 +0100

    Add files via upload

---
+++ sa_décomposition_en_facteurs_premiers.py
@@ -0,0 +1,38 @@
+﻿#!/usr/bin/python3
+# coding: utf-8
+
+
+def décomposition (nb_à_dec, indice, produits):
+    indice = 2
+    produits = “1”
+    if (est_premier (nb_à_dec)):
+        print (“Ce nombre n’a pas de diviseur autre que lui-même et un.”)
+        return False
+    else:
+        while (indice < nb_à_dec):
+            if (nb_à_dec % indice == 0):
+                produits = produits + “*” + str(indice)
+                nb_à_dec = int(nb_à_dec / indice)
+                indice = indice - 1
+                if (est_premier (nb_à_dec)):
+                    produits = produits + “*” + str(nb_à_dec)
+            indice = indice + 1
+    return produits
+##################################################
+def est_premier (nb):
+    nb_premier = True
+    for i in range (2, nb):
+        if (nb % i):
+            nb_premier = False
+       #   break
+       # else:
+            # Instruction(s) exécutée(s) seulement si la boucle n’est pas interrompue par l’instruction break
+         #   nb_premier = True
+    return nb_premier        
+##################################################
+nb = int(input(“Entrez un nombre à décomposer.”))
+
+
+if (décomposition(nb)):
+    print (“Une de ses décompositions en facteur(s) est : “, décomposition(nb)) 
+----------------------------------------------------------------- Résultat ---------------------------------------------------------------------------------
